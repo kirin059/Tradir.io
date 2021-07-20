5acec138eaecaa18ba67300cba3766f507bbe734
saebom <boma91@gmail.com>
2021-07-20 23:53:34 +0900

    Add: beerlist 페이지 레이아웃 구현 완료

--- src/Pages/BeerList.js
+++ src/Pages/BeerList.js
@@ -1,35 +1,52 @@
 import React from 'react';
-import Table from '@material-ui/core/Table';
-import TableHead from '@material-ui/core/TableHead';
-import TableBody from '@material-ui/core/TableBody';
-import TableRow from '@material-ui/core/TableRow';
-import TableCell from '@material-ui/core/TableCell';
+import { Steps } from 'antd';
+import { Table, TableHead, TableBody, TableRow, TableCell } from '@material-ui/core';
+import styled from "styled-components";
 
 const BeerList = () => {
+    const { Step } = Steps;
+    const ListContainer = styled.div`
+        margin: 0;
+        padding: 50px;
+        box-sizing: border-box;
+        width: 100vw;
+        height: 100vh;
+    `;
+    const Tables = styled(Table)`
+        margin-top: 100px;
+    `;
+
     return (
-        <div>
-            <Table>
+        <ListContainer>
+            <Steps current={1}>
+                <Step title="welcome" />
+                <Step title="Beer List" />
+                <Step title="Shopping Basket" />
+            </Steps>
+            <Tables>
                 <TableHead>
                 <TableRow>
                     <TableCell>No.</TableCell>
+                    <TableCell>Image</TableCell>
                     <TableCell>Name</TableCell>
                     <TableCell>Tag Line</TableCell>
-                    <TableCell>First Brewed</TableCell>
-                    <TableCell>Description</TableCell>
-                    <TableCell>Image</TableCell>
-                    <TableCell>abv</TableCell>
-                    <TableCell>ibu</TableCell>
-                    <TableCell>target_fg</TableCell>
-                    <TableCell>target_og</TableCell>
+                    <TableCell>Info</TableCell>
                 </TableRow>
                 </TableHead>
                 <TableBody>
-                {/* {customers.map(c => {
-                return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+                {/* {beers.map(a => {
+                return 
+                <TableRow key={id}>
+                    <TableCell align="left"> {i+1} </TableCell>
+                    <TableCell align="left"> {a.img} </TableCell>
+                    <TableCell align="left"> {a.name} </TableCell>
+                    <TableCell align="left"> {a.tagline} </TableCell>
+                    <TableCell align="left"> {a.info} </TableCell>
+                </TableRow>
                 })} */}
                 </TableBody>
-            </Table>
-        </div>
+            </Tables>
+        </ListContainer>
     );
 };
 
