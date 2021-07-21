5a5c89f24041073983b348a5650763ed7687a3b9
saebom <boma91@gmail.com>
2021-07-21 14:56:53 +0900

    Add: reducer생성 및 axios 데이터 받아오기

--- src/Modules/index.js
+++ src/Modules/index.js
@@ -1,13 +1,37 @@
 import { combineReducers } from "redux";
 import { all } from "redux-saga/effects";
-
+import axios from 'axios'
 //watcher saga -> actions -> worker saga
 // import loading from "./loading";
 import { enableES5 } from "immer";
 
 enableES5();
 
+
+
+// axios 로 데이터 받아오기
+const loadItem = async () => {
+  const items = await axios.get("https://api.punkapi.com/v2/beers")
+    .then(res => {
+      //console.log(res.data)
+      return res.data;
+    })
+    .catch(() => {
+      console.log('error')
+    });
+  return [items]
+};
+const beer = loadItem();
+
+function reducer(state = beer, action) {
+  return state
+}
+
+
+
+// 리듀서 생성
 const rootReducer = combineReducers({
+  // 리듀서 넣기
 });
 
 // export default rootReducer;
--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ import { rootSaga } from "./Modules";
 import createSagaMiddleware from "redux-saga";
 import { createStore, applyMiddleware, compose } from "redux";
 
-const sagaMiddleware = createSagaMiddleware();
+const sagaMiddleware = createSagaMiddleware();   // 미들웨어생성
 
+//스토어 생성(루트리듀서와 미들웨어 연결)
 export const store = createStore(
   rootReducer,
   compose(
